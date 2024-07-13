712bb34e63b09671c3113bbd1a596c3dfa7d5802
Elchin Hasanov <82273964+elchin-hasanov@users.noreply.github.com>
2024-07-13 09:18:03 +0400

    static files

--- ibspawn/settings.py
+++ ibspawn/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'main',
 ]
 
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -112,12 +114,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'main', 'static'),
+]
+STATIC_ROOT = BASE_DIR / "main" / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
