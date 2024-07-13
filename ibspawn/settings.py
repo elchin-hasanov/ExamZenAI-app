a7e85878efd77cfb37e4828e5e00f39e94c662d9
Elchin Hasanov <82273964+elchin-hasanov@users.noreply.github.com>
2024-07-13 09:31:08 +0400

    csrf2

--- ibspawn/settings.py
+++ ibspawn/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://examzen-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://examzen-production.up.railway.app']
 
 # Application definition
 
