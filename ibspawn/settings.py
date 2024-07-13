78becd47c6b807a332f8766734d6644426d2225a
Elchin Hasanov <82273964+elchin-hasanov@users.noreply.github.com>
2024-07-13 09:52:42 +0400

    corsheaders

--- ibspawn/settings.py
+++ ibspawn/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'corsheaders',
 ]
 
 
@@ -51,7 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
+CORS_ALLOWED_ORIGINS = ['*']
+
 
 ROOT_URLCONF = 'ibspawn.urls'
 
