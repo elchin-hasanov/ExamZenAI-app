069d002e7f026b465d3bd3aaace50c1a6fc3794c
Elchin Hasanov <82273964+elchin-hasanov@users.noreply.github.com>
2024-07-14 10:10:19 +0400

    cors

--- ibspawn/settings.py
+++ ibspawn/settings.py
@@ -16,8 +16,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'corsheaders',
-
 ]
 
 MIDDLEWARE = [
@@ -29,10 +27,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-
 ]
-CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'ibspawn.urls'
 
