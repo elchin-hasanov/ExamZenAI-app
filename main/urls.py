af7e7a62c7e06fa7492ad6c4d0e3fc655eeb93c7
Elchin Hasanov <82273964+elchin-hasanov@users.noreply.github.com>
2024-07-14 11:32:03 +0400

    html changes

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
-app_name = 'main'
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('test/', views.test, name='test'),
